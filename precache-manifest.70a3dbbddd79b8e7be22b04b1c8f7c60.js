e423cecd0a4da336a4d3c487bccf62cf2eed1381
MANUCHANDRAN94 <m4manu001@gmail.com>
2020-09-21 22:40:31 +0530

    Updates

---
+++ precache-manifest.70a3dbbddd79b8e7be22b04b1c8f7c60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d26c03fe25d079b814eb9ead97e0aac",
+    "url": "/FCC-React-calculator/index.html"
+  },
+  {
+    "revision": "22ee5a508bf64b9d0310",
+    "url": "/FCC-React-calculator/static/css/main.8c83fd25.chunk.css"
+  },
+  {
+    "revision": "3ba8ce8455d274225817",
+    "url": "/FCC-React-calculator/static/js/2.28d684e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FCC-React-calculator/static/js/2.28d684e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ee5a508bf64b9d0310",
+    "url": "/FCC-React-calculator/static/js/main.dc61f9e5.chunk.js"
+  },
+  {
+    "revision": "99541382066bc6a3dd3a",
+    "url": "/FCC-React-calculator/static/js/runtime-main.0f86398f.js"
+  }
+]);
